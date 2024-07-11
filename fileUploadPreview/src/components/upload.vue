50d30754ece3c423dc1fd20de6d2c9fa0128ba19
baixiaohui <1974089790@qq.com>
2024-07-11 23:41:33 +0800

    feat：文件上传组件

---
+++ fileUploadPreview/src/components/upload.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="upload-container">
+    <el-upload
+      v-model:file-list="fileList"
+      action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+      list-type="picture-card"
+      :on-preview="handlePictureCardPreview"
+      :on-remove="handleRemove"
+    >
+      <el-icon><Plus /></el-icon>
+    </el-upload>
+    <!-- 图片预览 -->
+    <el-dialog v-model="dialogVisible">
+      <img w-full :src="dialogImageUrl" alt="Preview Image" />
+    </el-dialog>
+  </div>
+</template>
+
+<script setup>
+import { ref, toRefs } from 'vue'
+import { Plus } from '@element-plus/icons-vue'
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'img'
+  }
+})
+const { type } = toRefs(props)
+
+
+const fileList = ref([])
+
+const dialogImageUrl = ref('')
+const dialogVisible = ref(false)
+
+const handleRemove = (uploadFile, uploadFiles) => {
+  console.log(uploadFile, uploadFiles)
+}
+
+const handlePictureCardPreview = (uploadFile) => {
+  dialogImageUrl.value = uploadFile.url
+  dialogVisible.value = true
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
